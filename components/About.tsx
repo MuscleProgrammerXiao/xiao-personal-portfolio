d2961c213f946021124e242aa0518e544c561d7b
superflashcx <895422334@qq.com>
2025-01-03 16:32:54 +0800

    翻译

--- components/About.tsx
+++ components/About.tsx
@@ -24,90 +24,32 @@ export default function About() {
       {activeLocale == "zh" ? (
         <div className="flex flex-col gap-2">
           <p>我是一个热爱运动，喜欢阅读和分享，脑子里总有产生一些奇思妙想的人。</p>
-          <p>我的理想是做一款AI应用，为此我不得不出卖自己的手指，靠努力敲代码赚钱吃饭，并在吃饱了撑的时候积极筹备这件事。</p>
+          <p>我的理想是有一家自己的公司和我主导开发的产品。为此我不得不出卖自己的手指，靠努力敲代码赚钱吃饭，并在吃饱了撑的时候积极筹备这件事。</p>
           <p>从2020年毕业至今，我经历了四家公司，从重庆来到深圳。在这段时间里我从一个小白逐渐融入到的社会和工作中。
           我喜欢阅读书籍并试图带入书中人物，揣摩他们的想法。并和不同年龄段的人交流我的心得。意外的是这让我交到了一些比我年长几十岁的朋友😂。</p>
           <p>目前我在为银行工作。在项目组中担任前端开发并负责部分产品建设。日常会负责一些项目的搭建，需求排期工作分配和部分代码编写。也会和
-          业务交流沟通，砍掉一些不合理的需求...也会思维发散，和分行一线人员积极交流产品，将产品持续的迭代和优化。</p>
+          业务交流沟通，砍掉一些不合理的需求...也会思维发散，和一线人员积极交流产品，将产品持续的迭代和优化。</p>
           <p>开发，管理和产品的工作糅杂在一起，时常让我心力憔悴，但这也让我收获了很多。正是有了这些生活和工作的经验，让我大部分时间都充满干劲（或是郁闷-.-）。</p>
         </div>
       ) : (
         <>
           <p className="mb-3">
-            My journey into programming kicked off during my undergrad in
-            <span className="font-medium italic">
-              {" "}
-              Digital Publishing
-            </span> at {}
-            <span className="italic underline">
-              <a
-                href="https://en.wikipedia.org/wiki/Wuhan_University"
-                target="_blank"
-              >
-                Wuhan University.
-              </a>
-            </span>{" "}
-            As a freshman, I got my feet wet with the basics of computer
-            science, databases, and Python. But what truly sparked my passion
-            was a course on semantic publishing—there, I used HTML and CSS to
-            create a webpage dedicated to my favorite figure skater,{" "}
-            <a
-              href="https://en.wikipedia.org/wiki/Yuzuru_Hanyu"
-              target="_blank"
-              className="italic underline"
-            >
-              Yuzuru Hanyu.{" "}
-            </a>
-            This experience opened my eyes to the charm of web development———
-            <span className="font-medium italic">
-              literally, you can build anything you envision with code.
-            </span>
+          I&#39;m someone who loves sports, enjoys reading and sharing, and always comes up with creative ideas.
+          </p>
+          <p className="mb-3">
+          My dream is to have my own company and products that I lead the development of. To achieve this, I have to &quot;sell&quot; my fingers, tirelessly typing code to make a living. When I’m not busy surviving, I actively prepare for making this dream a reality.
           </p>
 
           <p className="mb-3">
-            I sharpened my front-end development skills through internships at
-            Internships at Wuhan University&apos;s Big Data Institute and later
-            at{" "}
-            <span className="italic underline">
-              <a href="https://www.nio.com/careers" target="_blank">
-                {" "}
-                NIO Inc.
-              </a>
-            </span>{" "}
-            Working closely with developers, designers, testers, and product
-            managers, I loved the buzz of a team pulling together to make our
-            product better. And there’s nothing quite like the thrill of seeing
-            my own code being used by thousands—it’s what solidified my decision
-            to pursue a career in front-end development.
+          Since graduating in 2020, I&#39;ve worked at four companies and moved from Chongqing to Shenzhen. During this time, I gradually transitioned from a beginner to becoming more integrated into society and my work.
+          I enjoy reading books and trying to step into the shoes of the characters, exploring their thoughts. I also like sharing my insights with people of different ages. Surprisingly, this has led me to make friends with people decades older than me. 😂
           </p>
-
           <p className="mb-3">
-            I’m now doing my Masters in
-            <span className="font-medium italic"> Computing and IT</span> at the{" "}
-            <span className="italic underline">
-              <a href="https://www.st-andrews.ac.uk/" target="_blank">
-                University of St Andrews.{" "}
-              </a>
-            </span>
-            I thrive on programming challenges and enjoy working with teams to
-            solve complex problems. I specialize in technologies such as
-            <span className="font-medium italic"> React, Next.js and Vue</span>,
-            and have a solid understanding of{" "}
-            <span className="font-medium italic">
-              JavaScript, TypeScript and HTML/CSS
-            </span>
-            .{" "}
+          Here&#39;s the translation:
+          Currently, I work at a bank as a frontend developer, responsible for part of the product development. My daily tasks include setting up projects, scheduling tasks, work allocation, and writing some code. I also communicate with the business side, cutting down unreasonable requests, and brainstorm ideas by actively discussing with front-line staff to continuously iterate and optimize the product.
           </p>
-          <p>
-            In my spare time, I enjoy exploring new technologies and building
-            interesting projects. I also run my social media accounts on
-            platforms like Bilibili and Xiaohongshu, where I share tech tips and
-            tricks. And when I’m not at the computer, you’ll find me
-            <span className="font-medium italic">
-              {" "}
-              cooking up a storm, catching a movie, or keeping fit with regular
-              workouts.
-            </span>
+          <p className="mb-3">
+          Juggling development, management, and product responsibilities often leaves me mentally and physically exhausted, but it has also taught me a lot. These experiences in life and work keep me motivated most of the time (or sometimes frustrated -.-).
           </p>
         </>
       )}
